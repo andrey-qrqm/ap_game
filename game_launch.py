663013fab8f7b179f0d046b1044109f6c9b262ff
Andrey QRQM <andrey.madyarov@gmail.com>
2024-02-19 17:00:07 +0300

    added start cutscene

--- game_launch.py
+++ game_launch.py
@@ -1,5 +1,9 @@
 import game_threads
 import cutscene
 
-
+cutscene.cutscene(
+        text="Well, you've opened my game. Good Luck and try to win it",
+        songfile="song_1.wav",
+        imagefile="title.png",
+    )
 game_threads.start()
